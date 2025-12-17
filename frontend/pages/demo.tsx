3ed8b00eb344b7626d1be3b9e37c3c6d974d9270
eshaanag <agrawaleshaan12@gmail.com>
2025-12-17 22:56:39 +0530

    Add DemoPage component with iframe

---
+++ frontend/pages/demo.tsx
@@ -0,0 +1,13 @@
+export default function DemoPage() {
+  return (
+    <iframe
+      src="/demo/index.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+      title="Elyaitra Demo"
+    />
+  );
+}
