1c0918564343c809f0b53d7bbe5edb57694b0b23
eshaanag <agrawaleshaan12@gmail.com>
2025-12-17 23:39:18 +0530

    Delete frontend directory

--- frontend/pages/demo.tsx
+++
@@ -1,9 +0,0 @@
-export default function DemoPage() {
-  return (
-    <iframe
-      src="/demo/index.html"
-      style={{ width: "100%", height: "100vh", border: "none" }}
-      title="Demo"
-    />
-  );
-}
--- frontend/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Elyaitra is Live</h1>
-      <p>This is the homepage.</p>
-      <a href="/demo">Go to Demo</a>
-    </main>
-  );
-}
